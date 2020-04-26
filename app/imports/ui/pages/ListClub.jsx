0b70e80ea5098b26e5bc9db5dbe7468ee9f9bdd7
julianki-cs <60210330+julianki-cs@users.noreply.github.com>
2020-04-25 20:00:57 -1000

    Update ListClub.jsx

--- app/imports/ui/pages/ListClub.jsx
+++ app/imports/ui/pages/ListClub.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Segment } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import SimpleSchema from 'simpl-schema';
+import { _ } from 'meteor/underscore';
+import { AutoForm, SubmitField } from 'uniforms-semantic';
 import Club from '../components/Club';
 import { Clubs } from '../../api/club/Clubs';
+import { Interests } from '../../api/interests/Interests';
+import MultiSelectField from '../forms/controllers/MultiSelectField';
+
+const makeSchema = (clubInterests) => new SimpleSchema({
+  interest: { type: Array, label: 'Interests', optional: true },
+  'interest.$': { type: String, allowedValues: clubInterests },
+});
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListClubs extends React.Component {
@@ -16,8 +26,16 @@ class ListClubs extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const clubInterests = _.pluck(Interests.find().fetch(), 'interest');
+    const formSchema = makeSchema(clubInterests);
     return (
         <Container>
+          <AutoForm schema={formSchema} onSubmit={data => this.submit(data)} >
+            <Segment>
+              <MultiSelectField name='interests' showInlineError={true} placeholder={'Interests'}/>
+              <SubmitField value='Submit'/>
+            </Segment>
+          </AutoForm>
           <Header as="h2" textAlign="center" inverted>Clubs at UHM</Header>
           <Card.Group>
             {this.props.clubs.map((club, index) => <Club key={index} club={club} />)}
@@ -34,8 +52,9 @@ ListClubs.propTypes = {
 
 export default withTracker(() => {
   const subscription1 = Meteor.subscribe('Clubs');
+  const subscription2 = Meteor.subscribe('Interests');
   return {
     clubs: Clubs.find({}).fetch(),
-    ready: subscription1.ready(),
+    ready: subscription1.ready() && subscription2.ready(),
   };
 })(ListClubs);
