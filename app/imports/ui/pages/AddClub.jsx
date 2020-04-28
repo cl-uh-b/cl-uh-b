ce218113c6d74ba32f4b566960ddd4e4f28da67c
Tiffany Williams <60216343+tiffanywilliams@users.noreply.github.com>
2020-04-27 20:55:51 -1000

    Implemented basic add functionality, still needs tweeking

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -3,21 +3,38 @@ import { Grid, Segment, Header } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
 import swal from 'sweetalert';
 // import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
-import { Clubs, ClubSchema } from '../../api/club/Clubs';
+import SimpleSchema from 'simpl-schema';
+import { _ } from 'meteor/underscore';
+import { Clubs } from '../../api/club/Clubs';
+import { Interests } from '../../api/interests/Interests';
+import MultiSelectField from '../forms/controllers/MultiSelectField';
+// import { Submissions } from '../../api/submissions/Submissions';
+
+const makeSchema = (clubInterests) => new SimpleSchema({
+  clubName: { type: String, label: 'Club Name', optional: true },
+  interest: { type: Array, label: 'Interests', optional: true },
+  'interest.$': { type: String, allowedValues: clubInterests },
+  description: { type: String, label: 'Description', optional: true },
+  contact: { type: String, label: 'Contact', optional: true },
+  email: { type: String, label: 'Email', optional: true },
+  image: { type: String, label: 'Image', optional: true },
+});
 
 /** Renders the Page for adding a document. */
 class AddClub extends React.Component {
 
   /** On submit, insert the data. */
   submit(data, formRef) {
-    const { clubName, type, description, contact, email } = data;
-    Clubs.insert({ clubName, type, description, contact, email },
-      (error) => {
+    const { clubName, interest, description, contact, email, image } = data;
+    Clubs.insert({ clubName, interest, description, contact, email, image },
+        (error) => {
         if (error) {
           swal('Error', error.message, 'error');
         } else {
-          swal('Success', 'Item added successfully', 'success');
+          swal('Success', 'Item submitted successfully', 'success');
           formRef.reset();
         }
       });
@@ -26,22 +43,25 @@ class AddClub extends React.Component {
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     let fRef = null;
+    const clubInterests = _.pluck(Interests.find().fetch(), 'interest');
+    const formSchema = makeSchema(clubInterests);
     return (
         <Grid container centered>
           <Grid.Column>
             <Header as="h2" textAlign="center" inverted>Add Club</Header>
-            <AutoForm ref={ref => { fRef = ref; }} schema={ClubSchema} onSubmit={data => this.submit(data, fRef)} >
-              <Segment>
-                <TextField name='clubName'/>
-                <SelectField name='type'/>
-                <LongTextField name='description'/>
-                <Segment.Group horizontal>
-                  <Segment><TextField name='contact'/></Segment>
-                  <Segment><TextField name='email'/></Segment>
-                </Segment.Group>
-                <SubmitField value='Submit'/>
-                <ErrorsField/>
-              </Segment>
+              <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)} >
+                <Segment>
+                  <TextField name='clubName'/>
+                  <MultiSelectField name='interest'/>
+                  <LongTextField name='description'/>
+                  <Segment.Group horizontal>
+                    <Segment><TextField name='contact'/></Segment>
+                    <Segment><TextField name='email'/></Segment>
+                    <Segment><TextField name='image'/></Segment>
+                  </Segment.Group>
+                  <SubmitField value='Submit'/>
+                  <ErrorsField/>
+                </Segment>
             </AutoForm>
           </Grid.Column>
         </Grid>
@@ -49,4 +69,9 @@ class AddClub extends React.Component {
   }
 }
 
-export default AddClub;
+export default withTracker(() => {
+  const subscription = Meteor.subscribe('Interests');
+  return {
+    ready: subscription.ready(),
+  };
+})(AddClub);
