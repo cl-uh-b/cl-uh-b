0cbf515855324748f1454e3a012361a8c62188c5
kvndngyn <nk279@hawaii.edu>
2020-05-02 19:51:14 -1000

    created search function

--- app/imports/ui/pages/ListClub.jsx
+++ app/imports/ui/pages/ListClub.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card, Dropdown } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Dropdown, Form } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
@@ -11,9 +11,13 @@ import { Favorites } from '../../api/favorites/Favorites';
 
 class ListClubs extends React.Component {
 
-  state = { value: '' }
+  state = { value: '', search: '' }
 
   handleChange = (e, { value }) => this.setState({ value })
+
+  updateSearch(event) {
+    this.setState({ search: event.target.value });
+  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -23,6 +27,8 @@ class ListClubs extends React.Component {
   /** Render the page once subscriptions have been received. */
   renderPage() {
     const { value } = this.state;
+    /** Default Display */
+    let clubsOnPage = this.props.clubs;
 
     /** Assign key, text, values, semantic UI react dropdown */
     const options = this.props.interests.map(int => ({
@@ -32,29 +38,40 @@ class ListClubs extends React.Component {
     }));
 
     /** Filter to selected interest */
-    const originalList = this.props.clubs;
-    let filteredList = [];
     if (this.state.value !== '' || this.state.value.length === 0) {
       for (let i = 0; i < this.state.value.length; i++) {
-        const filtered = _.filter(originalList, (club) => club.interest.includes(this.state.value[i]));
-        filteredList = _.union(filteredList, filtered);
+        clubsOnPage = clubsOnPage.filter((club) => club.interest.includes(this.state.value[i]));
       }
     }
+
+    /** Filter to search results */
+    clubsOnPage = clubsOnPage.filter(
+        (club) => club.clubName.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1,
+    );
 
     return (
         <Container>
           <Header as="h2" textAlign="center">Clubs at UHM</Header>
-          <Dropdown
-              clearable fluid search selection multiple
-              placeholder='Select Interest'
-              value={value}
-              onChange={this.handleChange}
-              options={options}
-          />
+          <Form>
+            <Form.Group widths='equal'>
+              <Form.Input
+                  onChange={this.updateSearch.bind(this)}
+                  name='search'
+                  className='icon'
+                  icon='search'
+                  placeholder='Search Clubs'
+              />
+              <Form.Dropdown
+                  clearable search selection multiple
+                  placeholder='Select Interest'
+                  value={value}
+                  onChange={this.handleChange}
+                  options={options}
+              />
+          </Form.Group>
+          </Form>
           <Card.Group itemsPerRow={4} style={{ marginTop: '20px' }}>
-           {this.state.value.length === 0 ?
-             originalList.map((club, index) => <Club key={index} club={club} favorites={this.props.favorites}/>)
-             : filteredList.map((club, index) => <Club key={index} club={club} favorites={this.props.favorites}/>)}
+            {clubsOnPage.map((club, index) => <Club key={index} club={club} favorites={this.props.favorites}/>)}
           </Card.Group>
         </Container>
     );
