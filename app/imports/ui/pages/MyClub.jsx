9056e093390ee88a30a029aad220e56fd4a6fdb7
lekanh <60237335+lekanh@users.noreply.github.com>
2020-05-03 03:34:32 -1000

    Auto assign clubowners

--- app/imports/ui/pages/MyClub.jsx
+++ app/imports/ui/pages/MyClub.jsx
@@ -1,42 +1,65 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Button, Grid } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import ClubOwner from '../components/ClubOwner';
 import { Clubs } from '../../api/club/Clubs';
-
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class MyClub extends React.Component {
 
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-  render() {
-    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-  }
+    state = { redirect: false };
 
-  /** Render the page once subscriptions have been received. */
-  renderPage() {
-    return (
-        <Container>
-          <Header as="h2" textAlign="center">My Clubs</Header>
-          <Card.Group>
-            {this.props.clubs.map((club, index) => <ClubOwner key={index} club={club}/>)}
-          </Card.Group>
-        </Container>
-    );
-  }
+    add() {
+        this.setState({ redirect: true });
+    }
+
+    /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+    render() {
+        return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+    }
+
+    /** Render the page once subscriptions have been received. */
+    renderPage() {
+        if (this.state.redirect === false) {
+            return (
+                <Container>
+                    <Header as="h2" textAlign="center">My Clubs</Header>
+                    {this.props.clubs.length !== 0 ? <Card.Group>
+                            {this.props.clubs.map((club, index) => <ClubOwner key={index} club={club}/>)}
+                        </Card.Group> :
+                        <Grid centered>
+                            <Grid.Row>
+                                <p id='noClubs'>
+                                Sorry, It seems like you don&apos;t have any clubs yet.
+                                </p>
+                            </Grid.Row>
+                            <Grid.Row>
+                                <Button onClick={this.add.bind(this)}>Click here to create a new club!</Button>
+                            </Grid.Row>
+                        </Grid>
+                    }
+                </Container>
+            );
+        }
+
+        return (
+            <Redirect to='/add' />
+            );
+    }
 }
 
 MyClub.propTypes = {
-  clubs: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
+    clubs: PropTypes.array.isRequired,
+    ready: PropTypes.bool.isRequired,
 };
 
 export default withTracker(() => {
-  const subscription = Meteor.subscribe('MyClubs');
-  return {
-    clubs: Clubs.find({}).fetch(),
-    ready: subscription.ready(),
-  };
+    const subscription = Meteor.subscribe('MyClubs');
+    return {
+        clubs: Clubs.find({}).fetch(),
+        ready: subscription.ready(),
+    };
 })(MyClub);
