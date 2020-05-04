34724d146bcfc7ff67886e5bb29aa317e72064c5
kvndngyn <nk279@hawaii.edu>
2020-05-03 17:49:05 -1000

    implemented pagination to favorites, recomendations, listclubs, listclubsadmin

--- app/imports/ui/pages/FavoriteClubs.jsx
+++ app/imports/ui/pages/FavoriteClubs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Loader, Card, Statistic } from 'semantic-ui-react';
+import { Container, Loader, Card, Statistic, Pagination, Grid } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
@@ -8,8 +8,12 @@ import Club from '../components/Club';
 import { Clubs } from '../../api/club/Clubs';
 import { Favorites } from '../../api/favorites/Favorites';
 
-/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class FavoriteClubs extends React.Component {
+
+  state = { activePage: 1, clubsPerPage: 40 }
+
+  handlePaginationChange = (e, { activePage }) => this.setState({ activePage: activePage })
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -17,19 +21,44 @@ class FavoriteClubs extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const { activePage, clubsPerPage } = this.state;
     const favorite = _.pluck(this.props.favorites, 'FavoriteId');
-    const userFavorites = _.filter(this.props.clubs, (club) => _.contains(favorite, club._id));
+    let userFavorites = _.filter(this.props.clubs, (club) => _.contains(favorite, club._id));
     const totalFavorites = _.size(userFavorites);
+
+    /** Pagination */
+    const totalPages = Math.ceil(userFavorites.length / clubsPerPage);
+    userFavorites = userFavorites.slice(
+        (activePage - 1) * clubsPerPage,
+        (activePage - 1) * clubsPerPage + clubsPerPage,
+    );
+
+    /** Sort by letter */
+    userFavorites = userFavorites.sort((a, b) => (a.clubName > b.clubName ? 1 : -1));
     return (
         <Container fluid>
-          <Statistic horizontal label='Favorites' value={totalFavorites} />
-          <Card.Group>
-            {userFavorites.map((club, index) => <Club
-                key={index}
-                club={club}
-                favorites={this.props.favorites}
-            />)}
-          </Card.Group>
+          <Grid centered>
+            <Grid.Row>
+              <Statistic horizontal label='Favorites' value={totalFavorites} />
+            </Grid.Row>
+            <Grid.Row>
+              <Card.Group>
+                {userFavorites.map((club, index) => <Club
+                    key={index}
+                    club={club}
+                    favorites={this.props.favorites}
+                />)}
+              </Card.Group>
+            </Grid.Row>
+            <Grid.Row>
+              <Pagination
+                  activePage={activePage}
+                  totalPages={totalPages}
+                  siblingRange={1}
+                  onPageChange={this.handlePaginationChange}
+              />
+            </Grid.Row>
+          </Grid>
         </Container>
     );
   }
--- app/imports/ui/pages/ListClubs.jsx
+++ app/imports/ui/pages/ListClubs.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Loader, Card, Form } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Form, Grid, Pagination } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
@@ -11,13 +11,15 @@ import { Favorites } from '../../api/favorites/Favorites';
 
 class ListClubs extends React.Component {
 
-  state = { value: '', search: '' }
+  state = { value: '', search: '', activePage: 1, clubsPerPage: 40 }
 
   handleChange = (e, { value }) => this.setState({ value })
 
   updateSearch(event) {
     this.setState({ search: event.target.value });
   }
+
+  handlePaginationChange = (e, { activePage }) => this.setState({ activePage: activePage })
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -26,7 +28,7 @@ class ListClubs extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-    const { value } = this.state;
+    const { value, activePage, clubsPerPage } = this.state;
     /** Default Display */
     let clubsOnPage = this.props.clubs;
 
@@ -38,7 +40,9 @@ class ListClubs extends React.Component {
     }));
 
     /** Filter to selected interest */
-    if (this.state.value !== '' || this.state.value.length === 0) {
+    if (this.state.value === '' || this.state.value.length === 0) {
+      clubsOnPage = this.props.clubs;
+    } else {
       let filteredResults = [];
       for (let i = 0; i < this.state.value.length; i++) {
         const temp = this.props.clubs.filter((club) => club.interest.includes(this.state.value[i]));
@@ -52,10 +56,20 @@ class ListClubs extends React.Component {
         (club) => club.clubName.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1,
     );
 
+    /** Pagination */
+    const totalPages = Math.ceil(clubsOnPage.length / clubsPerPage);
+    clubsOnPage = clubsOnPage.slice(
+        (activePage - 1) * clubsPerPage,
+        (activePage - 1) * clubsPerPage + clubsPerPage,
+    );
+
+    /** Sort by letter */
+    clubsOnPage = clubsOnPage.sort((a, b) => (a.clubName > b.clubName ? 1 : -1));
+
     return (
-      <Container>
-        <Header as="h2" textAlign="center">Clubs at UHM</Header>
-        <Form>
+      <Container fluid style={{ padding: '0 290px' }}>
+        <Header as="h2" textAlign="center">Clusbs at UHM</Header>
+        <Form size='large'>
           <Form.Group widths='equal'>
             <Form.Input
                 onChange={this.updateSearch.bind(this)}
@@ -73,9 +87,21 @@ class ListClubs extends React.Component {
             />
         </Form.Group>
         </Form>
-        <Card.Group itemsPerRow={4} style={{ marginTop: '20px' }}>
-          {clubsOnPage.map((club, index) => <Club key={index} club={club} favorites={this.props.favorites}/>)}
-        </Card.Group>
+        <Grid centered>
+          <Grid.Row>
+            <Card.Group style={{ marginTop: '20px' }}>
+              {clubsOnPage.map((club, index) => <Club key={index} club={club} favorites={this.props.favorites}/>)}
+            </Card.Group>
+          </Grid.Row>
+          <Grid.Row>
+            <Pagination
+              activePage={activePage}
+              totalPages={totalPages}
+              siblingRange={1}
+              onPageChange={this.handlePaginationChange}
+            />
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
